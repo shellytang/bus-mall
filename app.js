940b6269733ba51eebcb362da8066ec693d6b3c0
Shelly Tang <shellytang@gmail.com>
2016-12-12 17:07:32 -0800

    created object constructor

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict';
+var left = document.getElementById('left');
+left.src = allProducts[randomIndexes[0]].src;
+
+var allProducts = [] //all image objects, like stores / images have names and filepath
+// var displayImages = document.getElementById('image_container');
+
+function ProductImage(name,filepath) {
+  this.name = name;
+  this.filepath = filepath;
+  this.views = 0;
+  this.clicks = 0;
+  allProducts.push(this);
+}
+new ProductImage('bag','images/bag.jpg');
+new ProductImage('banana','images/banana.jpg');
+new ProductImage('bathroom','images/bathroom.jpg');
+new ProductImage('boots','images/boots.jpg');
+new ProductImage('breakfast','images/breakfast.jpg');
+new ProductImage('bubblegum','images/bubblegum.jpg');
+new ProductImage('chair','images/chair.jpg');
+new ProductImage('cthulhu','images/cthulu.jpg');
+new ProductImage('dogduck','images/dog-duck.jpg');
+new ProductImage('dragon','images/dragon.jpg');
+new ProductImage('pen','images/pen.jpg');
+new ProductImage('petsweep','images/pet-sweep.jpg');
+new ProductImage('scissors','images/scissors.jpg');
+new ProductImage('shark','images/shark.jpg');
+new ProductImage('sweep','images/sweep.jpg');
+new ProductImage('tauntaun','images/tauntaun.jpg');
+new ProductImage('unicorn','images/unicorn.jpg');
+new ProductImage('usb','images/usb.jpg');
+new ProductImage('watercan','images/water-can.jpg');
+new ProductImage('wineglass','images/wineglass.jpg');
+
+function selectRandomImages() {
+  var randomIndexes = Math.floor(Math.random()*allProducts.length);
+  var left = document.getElementById('left');
+  left.src = allProducts[randomIndexes[0]].src;
+
+  randomIndexes = Math.floor(Math.random()*allProducts.length);
+  var center = document.getElementById('center');
+  center.src = allProducts[randomIndexes[1]].src;
+
+  randomIndexes = Math.floor(Math.random()*allProducts.length);
+  var right = document.getElementById('right');
+  right.src = allProducts[randomIndexes[2]].src;
+}
+selectRandomImages();
