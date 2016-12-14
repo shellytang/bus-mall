2b7b57de8b2eea54435668dfb80e08a88af826b6
Shelly Tang <shellytang@gmail.com>
2016-12-13 23:02:11 -0800

    changed row colors for chart

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
 var allProducts = []
 var newArray = []
 var oldArray = []
+var votes = [];
+var chartDrawn = false;
+var productChart;
 var displayImages = document.getElementById('image_container');
 var showResults = document.getElementById('show_results');
+var resultList = document.getElementById('result_list');
 var left = document.getElementById('left');
 var center = document.getElementById('center');
 var right = document.getElementById('right');
@@ -25,11 +29,9 @@ for (var i = 0; i < names.length; i++){
 }
 
 console.table(allProducts);
-
 function randNum() {   //generate random numbers for products
   return Math.floor(Math.random()*allProducts.length);
 }
-
 //generate array of three random numbers
 function makeArrayofThreeNumbers(){
   oldArray[0] = newArray[0];   //take the previous and store it in old array to use for comparison
@@ -42,27 +44,23 @@ function makeArrayofThreeNumbers(){
     newArray[0] = randNum();  //if duplicate, get random number again
     // console.log('fixed');
   }
-
   newArray[1] = randNum();   //fix this part
   while (newArray[1] === newArray[0] || newArray[1] === oldArray[0] || newArray[1] === oldArray[1] || newArray[1] === oldArray[2]) {
     newArray[1] = randNum();  //if duplicate, get random number again
     // console.log('caught dupes btw first and second numbers');
   }
-
   newArray[2] = randNum();
   while (newArray[2] === newArray[0] || newArray[2] === newArray[1] || newArray[2] === oldArray[0] || newArray[2] === oldArray[1] || newArray[2] === oldArray[2]) {
     newArray[2] = randNum();  //if duplicate, get random number again
     // console.log('caught dupes with the third number');
   }
 }
-
 // for (var j =0; j < 100; j++) {   //this tests if you are catching the dupes and new is moving to old
 //   makeArrayofThreeNumbers();
 // console.log('----NEW RUN-----');
 // console.log(oldArray, 'old array')
 // console.log(newArray,'new array');
 // }
-
 function showThreePics() {    //this will get three pics and tally the views
   makeArrayofThreeNumbers();
   left.src = allProducts[newArray[0]].filePath;
@@ -72,14 +70,11 @@ function showThreePics() {    //this will get three pics and tally the views
   right.src = allProducts[newArray[2]].filePath;
   allProducts[newArray[2]].views +=1;
 }
-
 showThreePics();
 
 function handleClick(event) {
-
   event.preventDefault(); //prevents reload of data
-  console.log(event.target.src + 'was clicked');
-
+  console.log(event.target.src + 'was clicked'); //alert if dont click on pics
   if (event.target.id === 'image_container') {
     return alert('Please click on a picture, not the background!');
   }
@@ -96,21 +91,15 @@ function handleClick(event) {
     allProducts[newArray[2]].clicks +=1;
     console.log(allProducts[newArray[2]]);
   }
-  //
   clickCounter += 1;
   console.log(clickCounter + ' total clicks so far')
   showThreePics();
-
-  if (clickCounter >= 5){  //change to 25 when done testing
-    // updateChartArrays(); //when test is over, push over the clicks to the array for chart
+  if (clickCounter >= 5){
     displayImages.removeEventListener('click', handleClick);
     showResults.style.visibility = 'visible';  //show results button appears at end of test
   }
 }
-
-var resultList = document.getElementById('result_list');
-
-function displayList() {    //shows list when ou click on the see results button
+function displayList() {    //shows list when you click on the see results button
   displayImages.innerHTML = '';
   for (var i = 0; i < allProducts.length; i++) {
     var liEl = document.createElement('li');
@@ -118,7 +107,6 @@ function displayList() {    //shows list when ou click on the see results button
     resultList.appendChild(liEl);
   }
 }
-
 //display a list of items and total clicks/views
 function handleResults() {
   console.log(event.target + 'was clicked');
@@ -126,16 +114,6 @@ function handleResults() {
   displayList();
   drawChart(); //new line
 }
-
-displayImages.addEventListener('click', handleClick); //eventListener for clicks
-showResults.addEventListener('click', handleResults);  //eventListener for results button
-showResults.style.visibility = 'hidden' //this hides the submit button before 25 clicks
-
-var votes = [];
-// var productListing = [];  //empty arrays to hold data for chart
-var chartDrawn = false;
-var productChart;
-
 function updateChartArrays() {
   for (var i = 0; i < allProducts.length; i++) {
     // productListing[i] = allProducts[i].productName;   do we need this?
@@ -143,6 +121,11 @@ function updateChartArrays() {
   }
 }
 
+displayImages.addEventListener('click', handleClick); //eventListener for clicks
+showResults.addEventListener('click', handleResults);  //eventListener for results button
+showResults.style.visibility = 'hidden' //this hides the submit button before 25 clicks
+
+//chart stuff below
 function drawChart () {
   var ctx = document.getElementById('result_chart').getContext('2d');
   productChart = new Chart(ctx, {
@@ -150,15 +133,53 @@ function drawChart () {
     data: {
       labels: names,
       datasets: [{
-        label: 'Number of Clicks',
+        label: 'Number of Votes',
         data: votes,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)'
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)'
         ],
         borderColor: [
-          'rgba(255, 159, 64, 1)'
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)',
+          'rgba(36, 123, 160, 1)'
         ],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
@@ -173,7 +194,6 @@ function drawChart () {
   });
   chartDrawn = true;
 }
-
 if (chartDrawn) {
   productChart.update();
 }
